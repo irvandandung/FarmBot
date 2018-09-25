5f70b466c2234c907c148a436358dcd1e93f499a
kharisazhar <muhammadkharisazhar149a@gmail.com>
2018-09-25 16:00:08 +0700

    create pengusir burung act

---
+++ app/src/main/java/com/ghootenk/user/farmbot/PengusirBurungActivity.java
@@ -0,0 +1,13 @@
+package com.ghootenk.user.farmbot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PengusirBurungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pengusir_burung);
+    }
+}
