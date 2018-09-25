903644773b39d314c19bd489f8eab85ce7edf21b
kharisazhar <muhammadkharisazhar149a@gmail.com>
2018-09-25 20:21:25 +0700

    added floating action button

--- app/src/main/java/com/ghootenk/user/farmbot/AlarmActivity.java
+++ app/src/main/java/com/ghootenk/user/farmbot/AlarmActivity.java
@@ -1,6 +1,8 @@
 package com.ghootenk.user.farmbot;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +13,15 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     public void backpestisida(View view) {
